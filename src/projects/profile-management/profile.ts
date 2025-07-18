6777c709bf4689c1e4b4472a7a64f9f0fdd7613a
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-07-18 23:23:01 +0600

    Learned interface and type

--- src/projects/profile-management/profile.ts
+++ src/projects/profile-management/profile.ts
@@ -12,3 +12,15 @@ interface Author extends User {
 }
 
 type Status = "active" | "inactive" | "banned";
+
+const author1: Author = {
+  id: 1,
+  username: "sheikh saiyam",
+  email: "saiyamprogrammer@gmail.com",
+  bio: "A Web Developer",
+  posts: ["Post 1", "Post 2", "Post 3", "Post 4", "Post 5"],
+};
+
+const accountStatus: Status = "active";
+
+// Learned Interface and Type from this project
