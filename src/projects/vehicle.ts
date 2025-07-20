8add59c953d2d6a42ba670ce3f992c4b5be9c108
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-07-20 16:19:44 +0600

    Learn OOP topic - classesand implements more deeply

--- src/projects/vehicle.ts
+++ src/projects/vehicle.ts
@@ -1,2 +1,27 @@
 // Vehicle Management System - Project
 
+interface Vehicle {
+  brand: string;
+  speed: number;
+  move(): void;
+}
+
+class Car implements Vehicle {
+  constructor(public brand: string, public speed: number) {}
+
+  move() {
+    console.log(`${this.brand} is moving on speed ${this.speed} km/h`);
+  }
+}
+
+const car = new Car("Tesla", 180);
+car.move();
+
+class ElectricCar extends Car {
+  charge() {
+    console.log(`${this.brand} is charging`);
+  }
+}
+
+const electricCar = new ElectricCar("Tesla", 180);
+electricCar.charge();
