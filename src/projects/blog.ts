e8ef42e388a71b92611739fe91ab6b31b1feb9ff
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-07-21 23:28:38 +0600

    Practing TypeScript enums more by a blog status project

--- src/projects/blog.ts
+++ src/projects/blog.ts
@@ -1,2 +1,39 @@
-// Blog Post Status - Project 
+// Blog Post Status - Project
 
+enum PostStatus {
+  draft = "DRAFT",
+  published = "PUBLISHED",
+  archived = "ARCHIVED",
+}
+
+interface POST {
+  title: string;
+  content: string;
+  status: PostStatus;
+}
+const post1: POST = {
+  title: "Learning typescript enums",
+  content:
+    "Learning TypeScript Enums: Numeric and string enums and Const enums and Use-cases",
+  status: PostStatus.published,
+};
+
+const post2: POST = {
+  title: "Learning typescript enums.",
+  content:
+    "Learning TypeScript Enums: Numeric and string enums and Const enums and Use-cases...",
+  status: PostStatus.draft,
+};
+
+const post3: POST = {
+  title: "Learning typescript enums..",
+  content:
+    "Learning TypeScript Enums: Numeric and string enums and Const enums and Use-cases.....",
+  status: PostStatus.archived,
+};
+
+console.log({
+  status1: post1.status,
+  status2: post2.status,
+  status3: post3.status,
+});
